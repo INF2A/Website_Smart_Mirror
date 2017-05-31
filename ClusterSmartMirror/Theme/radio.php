faf5b0bd5bfe99b130f056efd703b553591f7d23
RemcoEding <remco_eding11@hotmail.com>
2017-05-31 14:03:28 +0200

    Radio page updated

--- ClusterSmartMirror/Theme/radio.php
+++ ClusterSmartMirror/Theme/radio.php
@@ -16,6 +16,15 @@ else
           	<h3><i class="fa fa-angle-right"></i> Radio</h3>
           	<div class="row mt">
           		<div class="col-lg-12">
+                            <form class="form-login" method="POST" action="radio.php">
+                                <select>
+                                    <option value="Radio538">Radio 538</option>
+                                    <option value="SlamFM">Slam Fm</option>
+                                    <option value="Veronica">Veronica</option>
+                                    <option value="QMusic">QMusic</option>
+                                    <option value="SkyRadio">Skyradio</option>
+                                    <option value="Radio2">Radio 2</option>
+                                </select>
           		<p>Place your content here.</p>
 </html>
         
