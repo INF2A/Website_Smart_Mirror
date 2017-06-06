3c529832a3e77f46046083c075b0a51ce6fc56ed
RemcoEding <remco_eding11@hotmail.com>
2017-06-06 13:24:36 +0200

    Radio page now shows which one is your favorite and you can change your favorite radion station

--- ClusterSmartMirror/Theme/radio.php
+++ ClusterSmartMirror/Theme/radio.php
@@ -11,26 +11,42 @@ else
     include ('layout.php');
     include ('database.php');
    
+    if (isset($_POST['update'])) 
+    {
+        $selectedradio = $_POST['channels'];
+        $updatefavorite = mysqli_query($connection, "UPDATE radio_fav c set User_ID = ". $id. ", Channel_ID = ". $selectedradio);
+    }
     ?>
 <html>
     <section id="main-content">
           <section class="wrapper site-min-height">
           	<h3><i class="fa fa-angle-right"></i> Radio</h3>
           	<div class="row mt">
-          		<div class="col-lg-12">                            
+          		<div class="col-lg-12"> 
+                        <p><?php
+                            $favorite = mysqli_query($connection, "SELECT Name FROM channel INNER JOIN radio_fav ON channel.ID = radio_fav.Channel_ID WHERE radio_fav.User_ID =" . $id);
+                            while($row = mysqli_fetch_array($favorite)) {
+
+	                      echo "Your favorite radio station is <b>" .$row['Name'] . "</b><br />";
+
+                            }
+                        ?>
+                        </p>
           		<p><form method="POST" action="radio.php">
-                                <select>
+                                <select name="channels">
                                     <?php 
                                         
-                                    $query = mysqli_query($connection, "select * from channel");
+                                    $channels = mysqli_query($connection, "select * from channel");
     
-                                    while($row = mysqli_fetch_array($query)) {
+                                    while($row = mysqli_fetch_array($channels)) {
 
                                         echo "<option value='". $row['ID'] ."'>". $row['Name'] ."</option>";
                                     
                                     }
                                     ?>
-                                </select></p>
+                                </select><br /><br />
+                                <input type="submit" name="update" value="Change favorite">
+                        </p>
 </html>
         
 <?php
--- ClusterSmartMirror/Theme/session.php
+++ ClusterSmartMirror/Theme/session.php
@@ -5,9 +5,10 @@
     // Storing Session
     $user_check=$_SESSION['login_user'];
     // SQL Query To Fetch Complete Information Of User
-    $ses_sql=mysqli_query($connection, "select username from user where Username='$user_check'");
+    $ses_sql=mysqli_query($connection, "select * from user where Username='$user_check'");
     $row = mysqli_fetch_assoc($ses_sql);
-    $login_session =$row['username'];
+    $id = $row['ID'];
+    $login_session =$row['Username'];
     $login_session = ucfirst($login_session);
     if(!isset($login_session))
     {
