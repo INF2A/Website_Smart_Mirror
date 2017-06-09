1243ae4a7715ede6384ebb80fc313f464bbab1e3
RemcoEding <remco_eding11@hotmail.com>
2017-06-09 14:08:24 +0200

    news page shows available news sites

--- ClusterSmartMirror/Theme/news.php
+++ ClusterSmartMirror/Theme/news.php
@@ -16,7 +16,21 @@ else
           	<h3><i class="fa fa-angle-right"></i> News</h3>
           	<div class="row mt">
           		<div class="col-lg-12">
-          		<p>Place your content here.</p>
+                            <p><form method="POST" action="news.php">
+                                <select name="newssources">
+                                    <?php 
+                                        
+                                    $newssources = mysqli_query($connection, "SELECT DISTINCT SUBSTRING_INDEX(Name, ' ', 1) as Name FROM news_pref_item");
+    
+                                    while($row = mysqli_fetch_array($newssources)) {
+
+                                        echo "<option value='". $row['Name'] ."'>". $row['Name'] ."</option>";
+                                    
+                                    }
+                                    ?>
+                                </select><br /><br />
+                                <input type="submit" name="searchnews" value="Show subsites">
+                            </p>
 </html>
         
 <?php
