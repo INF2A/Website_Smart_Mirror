00bca51c313186bf2dee755ae1e29cf966264a36
RemcoEding <remco_eding11@hotmail.com>
2017-06-09 10:36:18 +0200

    wifi page now receives data from database

--- ClusterSmartMirror/Theme/wifi.php
+++ ClusterSmartMirror/Theme/wifi.php
@@ -9,6 +9,7 @@ else
 {
     $page = "wifi";
     include ('layout.php');
+    include ('database.php')
     ?>
 <html>
     <section id="main-content">
@@ -16,7 +17,21 @@ else
           	<h3><i class="fa fa-angle-right"></i> WiFi settings</h3>
           	<div class="row mt">
           		<div class="col-lg-12">
-          		<p>Place your content here.</p>
+                            <p>
+                            <form method="POST" action="wifi.php">
+                                <?php
+                                    
+                                    $getwifisettings = mysqli_query($connection, "SELECT * FROM wifi_settings WHERE User_ID = " . $id);
+                                    while($row = mysqli_fetch_array($getwifisettings)) {
+
+                                         echo "SSID:<input type='text' name='ssid' value='" .$row['ssid'] . "'><br /><br />";
+                                         echo "Password: <input type='text' name='password' value='" .$row['password'] . "'><br />";
+
+                                    }
+                                
+                                ?>
+                            </form>
+                            </p>
 </html>
         
 <?php
