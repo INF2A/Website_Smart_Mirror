89f714d712903e7a2b2a89fb623badd73aa6d364
RemcoEding <remco_eding11@hotmail.com>
2017-06-12 11:20:54 +0200

    added the possibility to edit your wifi settings

--- ClusterSmartMirror/Theme/wifi.php
+++ ClusterSmartMirror/Theme/wifi.php
@@ -9,7 +9,14 @@ else
 {
     $page = "wifi";
     include ('layout.php');
-    include ('database.php')
+    include ('database.php');
+    
+    if(isset($_POST['changewifi']))
+    {
+        $ssid = $_POST['ssid'];
+        $ssidpassword = $_POST['ssidpassword'];
+        $updatewifi = mysqli_query($connection, "UPDATE wifi_settings set User_ID = ". $id. ", ssid = '". $ssid. "', password = '".$ssidpassword."'");
+    }
     ?>
 <html>
     <section id="main-content">
@@ -24,12 +31,13 @@ else
                                     $getwifisettings = mysqli_query($connection, "SELECT * FROM wifi_settings WHERE User_ID = " . $id);
                                     while($row = mysqli_fetch_array($getwifisettings)) {
 
-                                         echo "SSID:<input type='text' name='ssid' value='" .$row['ssid'] . "'><br /><br />";
-                                         echo "Password: <input type='text' name='password' value='" .$row['password'] . "'><br />";
+                                         echo "<p>SSID:<input type='text' name='ssid' value='" .$row['ssid'] . "'></p>";
+                                         echo "<p>Password: <input type='text' name='ssidpassword' value='" .$row['password'] . "'></p>";
 
                                     }
                                 
                                 ?>
+                                <p><input type="submit" name="changewifi" value="Safe changes"></p>
                             </form>
                             </p>
 </html>
