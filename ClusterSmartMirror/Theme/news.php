5187434a2016d3e972fb534c1df9f11d036add72
RemcoEding <remco_eding11@hotmail.com>
2017-06-12 10:38:29 +0200

    News site now updates database

--- ClusterSmartMirror/Theme/news.php
+++ ClusterSmartMirror/Theme/news.php
@@ -9,6 +9,13 @@ else
 {
     $page = "news";
     include ('layout.php');
+    include ('database.php');
+    
+    if (isset($_POST['selectnews']))
+    {
+        $selectednews = $_POST['newssubject'];
+        $updatenews = mysqli_query($connection, "UPDATE news_pref set User_ID = ". $id. ", News_pref_item_ID = ". $selectednews);
+    }
     ?>
 <html>
     <section id="main-content">
@@ -17,12 +24,12 @@ else
           	<div class="row mt">
           		<div class="col-lg-12">
                             <p><form method="POST" action="news.php">
-                                <select name="newssources">
+                                <select name="newssource">
                                     <?php 
                                         
-                                    $newssources = mysqli_query($connection, "SELECT DISTINCT SUBSTRING_INDEX(Name, ' ', 1) as Name FROM news_pref_item");
+                                    $getnewssource = mysqli_query($connection, "SELECT DISTINCT SUBSTRING_INDEX(Name, ' ', 1) as Name FROM news_pref_item");
     
-                                    while($row = mysqli_fetch_array($newssources)) {
+                                    while($row = mysqli_fetch_array($getnewssource)) {
 
                                         echo "<option value='". $row['Name'] ."'>". $row['Name'] ."</option>";
                                     
@@ -30,7 +37,32 @@ else
                                     ?>
                                 </select><br /><br />
                                 <input type="submit" name="searchnews" value="Show subsites">
-                            </p>
+                            </p><br />
+                            <?php 
+                            
+                                if(isset($_POST['searchnews']))
+                                {
+                            ?>
+                            
+                            <p><form method="POST" action="news.php">
+                                 <select name="newssubject">
+                                    <?php 
+                                    $newssource = $_POST['newssource'];
+                                    $getnewssubject = mysqli_query($connection, "SELECT * FROM news_pref_item WHERE Name LIKE '%" .$newssource. "%'");
+    
+                                    while($row = mysqli_fetch_array($getnewssubject)) {
+
+                                        echo "<option value='". $row['ID'] ."'>". $row['Name'] ."</option>";
+                                    
+                                    }
+                                    ?>
+                                </select><br /><br />
+                                <input type="submit" name="selectnews" value="Select site">
+                            
+                            <?php
+                                }
+                            
+                            ?>
 </html>
         
 <?php
