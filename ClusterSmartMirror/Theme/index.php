59d9bab76ff7270ddc9f460d9f49d37a4b2fc554
RemcoEding <remco_eding11@hotmail.com>
2017-06-12 13:04:08 +0200

    added a table which shows all current settings

--- ClusterSmartMirror/Theme/index.php
+++ ClusterSmartMirror/Theme/index.php
@@ -17,53 +17,133 @@ else
           	<div class="row mt">
           		<div class="col-lg-12">
           		<p></p>
-                        <?php              
-                                                        
-                            $selectedtimezone = mysqli_query($connection, "SELECT time_zone FROM time_zones INNER JOIN time ON time_zones.ID = time.Timezone_ID WHERE time.User_ID = " . $id);
-                            while($row = mysqli_fetch_array($selectedtimezone)) {
+                        <table class="table table-striped table-advance table-hover">
+                            <h4><i class="fa fa-angle-right"></i> Your settings</h4>
+                            <hr>
+                              <thead>
+                              <tr>
+                                  <th><i class="fa fa-bullhorn"></i> Api</th>
+                                  <th class="hidden-phone"><i class="fa fa-question-circle"></i> Settings</th>
+                                  <th></th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                              <tr>
+                                  <td><a href="time.php">Time</a></td>
+                                  <td class="hidden-phone">
+                                        <?php
+                                      
+                                        $selectedtimezone = mysqli_query($connection, "SELECT time_zone FROM time_zones INNER JOIN time ON time_zones.ID = time.Timezone_ID WHERE time.User_ID = " . $id);
+                                        while($row = mysqli_fetch_array($selectedtimezone)) {
 
-	                      echo "<p>Your time zone is <b>" .$row['time_zone'] . "</b></p>";
+                                            echo $row['time_zone'];
 
-                            }
+                                        }
+                                      
+                                      ?>
+                                      
+                                  </td>
+                                  <td>
+                                      <a href="time.php"><button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+                                  </td>
+                              </tr>
+                              <tr>
+                                  <td>
+                                      <a href="weather.php">
+                                          Weather
+                                      </a>
+                                  </td>
+                                  <td class="hidden-phone">
+                                        <?php
+                                      
+                                        $selectedmetric = mysqli_query($connection, "SELECT Name FROM weather_pref INNER JOIN weather ON weather_pref.ID = weather.Weather_pref_ID WHERE weather.User_ID =" . $id);
+                                        while($row = mysqli_fetch_array($selectedmetric)) {
+
+                                            echo "System: ".$row['Name'];
+
+                                        }
                             
-                            $selectedmetric = mysqli_query($connection, "SELECT Name FROM weather_pref INNER JOIN weather ON weather_pref.ID = weather.Weather_pref_ID WHERE weather.User_ID =" . $id);
-                            while($row = mysqli_fetch_array($selectedmetric)) {
+                                        $selectedlocation = mysqli_query($connection, "SELECT location FROM weather WHERE User_ID =" .$id);
+                                        while($row = mysqli_fetch_array($selectedlocation)) {
 
-	                      echo "<p>Your prefered system for measuring temperature is <b>" .$row['Name'] . "</b>";
+                                            echo "<br>Locatie: ".$row['location'];
 
-                            }
-                            
-                            $selectedlocation = mysqli_query($connection, "SELECT location FROM weather WHERE User_ID =" .$id);
-                            while($row = mysqli_fetch_array($selectedlocation)) {
+                                        }
+                                      
+                                      ?>
+                                      
+                                  </td>
+                                  <td>
+                                      <a href="weather.php"><button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+                                  </td>
+                              </tr>
+                              <tr>
+                                  <td>
+                                      <a href="news.php">
+                                          News
+                                      </a>
+                                  </td>
+                                  <td class="hidden-phone">
+                                        <?php
+                                      
+                                        $selectednews = mysqli_query($connection, "SELECT Name FROM news_pref_item INNER JOIN news_pref ON news_pref_item.ID = news_pref.News_pref_item_ID WHERE news_pref.User_ID =" . $id);
+                                        while($row = mysqli_fetch_array($selectednews)) {
 
-	                      echo " and your location is <b>" .$row['location'] . "</b></p>";
+                                            echo $row['Name'];
 
-                            }
-                            
-                            $selectednews = mysqli_query($connection, "SELECT Name FROM news_pref_item INNER JOIN news_pref ON news_pref_item.ID = news_pref.News_pref_item_ID WHERE news_pref.User_ID =" . $id);
-                            while($row = mysqli_fetch_array($selectednews)) {
+                                        }
+                                      
+                                      ?>
+                                      
+                                  </td>
+                                  <td>
+                                      <a href="news.php"><button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+                                  </td>
+                              </tr>
+                              <tr>
+                                  <td>
+                                      <a href="radio.php">
+                                          Radio
+                                      </a>
+                                  </td>
+                                  <td class="hidden-phone">
+                                        <?php
+                                      
+                                        $selectedradio = mysqli_query($connection, "SELECT Name FROM channel INNER JOIN radio_fav ON channel.ID = radio_fav.Channel_ID WHERE radio_fav.User_ID =" . $id);
+                                        while($row = mysqli_fetch_array($selectedradio)) {
 
-	                      echo "<p>Your prefered news source is <b>" .$row['Name'] . "</b></p>";
+                	                    echo $row['Name'];
 
-                            }
-                            
-                            $selectedradio = mysqli_query($connection, "SELECT Name FROM channel INNER JOIN radio_fav ON channel.ID = radio_fav.Channel_ID WHERE radio_fav.User_ID =" . $id);
-                            while($row = mysqli_fetch_array($selectedradio)) {
+                                        }
+                                      
+                                      ?>
+                                      
+                                  </td>
+                                  <td>
+                                      <a href="radio.php.php"><button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+                                  </td>
+                              </tr>
+                              <tr>
+                                  <td><a href="wifi.php">Wifi</a></td>
+                                  <td class="hidden-phone">
+                                        <?php
+                                      
+                                        $selectedwifi = mysqli_query($connection, "SELECT * FROM wifi_settings WHERE User_ID =" . $id);
+                                        while($row = mysqli_fetch_array($selectedwifi)) {
 
-	                      echo "<p>Your favorite radio station is <b>" .$row['Name'] . "</b></p>";
+                                            echo "SSID: <b>".$row['ssid'] . "</b><br> Password: <b>" .$row['password']."</b>";
 
-                            }
-                            
-                            $selectedwifi = mysqli_query($connection, "SELECT * FROM wifi_settings WHERE User_ID =" . $id);
-                            while($row = mysqli_fetch_array($selectedwifi)) {
-
-	                      echo "<p>Your SSID is <b>" .$row['ssid'] . "</b> and your Password is <b>" .$row['password'] ."</b></p>";
-
-                            }
-                            
-                        
-                        
-                        ?>
+                                        }
+                                      
+                                      ?>
+                                      
+                                  </td>
+                                  <td>
+                                      <a href="wifi.php"><button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+                                  </td>
+                              </tr>
+                              </tbody>
+                          </table>
 </html>
         
 <?php
